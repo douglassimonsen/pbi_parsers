c7e9d8728e1dd1243822eea982f9e527c58694a7
Matthew Hamilton <mwhamilton6@gmail.com>
2025-07-01 13:20:30 -0500

    Added variable expresions

--- parser/pq/exprs/__init__.py
+++ parser/pq/exprs/__init__.py
@@ -4,6 +4,7 @@ from ._base import Expression
 from .function import FunctionExpression
 from .keyword import KeywordExpression
 from .literal_string import LiteralStringExpression
+from .variable import VariableExpression
 
 if TYPE_CHECKING:
     from ..parser import Parser
@@ -11,6 +12,7 @@ EXPRESSION_HIERARCHY: tuple[type[Expression], ...] = (
     FunctionExpression,
     LiteralStringExpression,
     KeywordExpression,
+    VariableExpression,
 )
 
 
@@ -22,7 +24,6 @@ def any_expression_match(parser: "Parser", skip_first: int = 0) -> Expression | 
     for expr in EXPRESSION_HIERARCHY[skip_first:]:
         if match := expr.match(parser):
             return match
-    breakpoint()
     return None
 
 
