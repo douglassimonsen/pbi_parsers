9820f638fd7de43d5b09c59217f51cd914190497
gitea <a@a.com>
2025-07-16 19:25:25 -0500

    Fixed a performance issue where the same expression was being evaluated a lot of times, now caching which has made a 1 hour run instantaneous

--- parser/exprs/_utils.py
+++ parser/exprs/_utils.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, ParamSpec, TypeVar
 
 from ._base import Expression
 
@@ -18,17 +18,27 @@ def or_match(
     return None
 
 
-from typing import ParamSpec, TypeVar
-
 P = ParamSpec("P")  # Represents the parameters of the decorated function
 R = TypeVar("R")  # Represents the return type of the decorated function
+
+
+cache: dict[tuple[int, int], tuple[Expression | None, int]] = {}
 
 
 def scanner_reset(func: Callable[P, R]) -> Callable[P, R]:
     def scanner_reset_inner(*args: P.args, **kwargs: P.kwargs) -> R:
         parser: "Parser" = args[1]  # type: ignore
         idx = parser.index
+
+        # Speed up of a bazillion
+        cached_val, cached_index = cache.get((idx, id(func)), (None, -1))
+        if cached_val is not None:
+            parser.index = cached_index
+            return cached_val  # type: ignore
+
         ret = func(*args, **kwargs)
+
+        cache[(idx, id(func))] = (ret, parser.index)  # type: ignore
         if ret is None:
             parser.index = idx
         return ret
